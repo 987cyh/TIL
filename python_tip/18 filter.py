3cb2c3189f5fcebb605fbc1c0da4fde07bb2b2cf
987cyh <987cyh@gmail.com>
2023-03-10 17:51:21 +0900

    filter

--- python_tip/18 filter.py
+++ python_tip/18 filter.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-□ 참고 : https://076923.github.io/posts/Python-21/, https://kibua20.tistory.com/194
+□ 참고 : https://076923.github.io/posts/Python-21/
 □ 기능 : filter
 □ 목적 : filter 기능 정리
 """
@@ -15,3 +15,14 @@ def func(x):
         return x - 100
 
 print(list(filter(func, range(-5, 5))))
+#-----------------------------------------------------
+def func(x):
+    if (x / 2) - 1 > 0:
+        return True
+    elif x % 2 == 0:
+        return True
+    else:
+        return False
+
+print(list(filter(func, range(-5, 5))))
+#%%
