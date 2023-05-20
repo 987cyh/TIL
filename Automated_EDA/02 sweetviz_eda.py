86487c5f87cc6a52a030ab2d91ce2d8693f0231d
987cyh <987cyh@gmail.com>
2023-05-20 18:01:10 +0900

    sweetviz

---
+++ Automated_EDA/02 sweetviz_eda.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+□ 참고 : https://docs.dataprep.ai/installation.html
+□ 기능 : sweetviz
+□ 목적 : sweetviz 학습(Automated EDA)
+"""
+#%%
+# package
+import numpy as np
+import pandas as pd
+import sweetviz as sv
+#%%
+# data load
+train_df = pd.read_csv("titanic_train.csv")
+test_df = pd.read_csv("titanic_test.csv")
+
+
+# single data EDA
+train_report = sv.analyze(train_df)
+train_report.show_html('sweetviz_train_eda.html')
+
+test_report = sv.analyze(test_df)
+test_report.show_html('sweetviz_train_eda.html')
+
+#%%
+## comparision datasets
+comparison_report = sv.compare(train_df, test_df)
+comparison_report.show_html('sweetviz_train_eda.html')
+
+comparison_report_y = sv.compare([train_df,"Train"], [test_df,"Test"], "Survived")
+comparison_report_y.show_html('sweetviz_train_eda_y.html')
+#%%
